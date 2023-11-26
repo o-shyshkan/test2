8d475c4c4947a2b6649b1ef111c9ff537a87ad82
o-shyshkan <o.shyshkan@gmail.com>
2023-11-26 11:34:16 +0200

    Remove files mvnw and .mvn/wrapper/

--- src/main/java/com/example/test2/AggregatingUsersApplication.java
+++ src/main/java/com/example/test2/AggregatingUsersApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
-public class Test2Application {
+public class AggregatingUsersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Test2Application.class, args);
+		SpringApplication.run(AggregatingUsersApplication.class, args);
 	}
 
 }
